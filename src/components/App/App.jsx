b71dfe8ea7c9c099385288ae2ec072f205a444fd
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-12-12 00:01:36 +0100

    fixed total

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,73 +4,49 @@ import FeedbackVidgetControls from '../feedback-vidget/feedback-vidget-controls'
 import FeedbackVidgetStatistic from '../feedback-vidget/feedback-vidget-statistic';
 import FeedbackVidgetNotification from '../feedback-vidget/feedback-vidget-notification';
 
-
 export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  
-  
+
   const handleIncrementGood = () => {
-   setGood(state => state + 1
-      );
-    countTotalFeedback();
+    setGood(state => state + 1);
   };
 
- 
   const handleIncrementNeutral = () => {
-     setNeutral(state => 
-       state + 1 
-     );
-     countTotalFeedback();
+    setNeutral(state => state + 1);
   };
-  
-   
+
   const handleIncrementBad = () => {
-     setBad(state => state + 1);
-     countTotalFeedback();
+    setBad(state => state + 1);
   };
 
+  const total = good + neutral + bad;
 
-
-  const countTotalFeedback = () => {
-    setTotal(state => state + 1);
+  const countPositiveFeedbackPercentage = () => {
+    return total !== 0 ? Math.round((good / total) * 100) : 0;
   };
-
-
- const countPositiveFeedbackPercentage = () => {
-    return total !== 0
-       ? Math.round((good / total) * 100)
-       : 0;
-  };
-
-
 
   return (
-  <FeedbackVidgetContainer>
-        <FeedbackVidgetControls
-          title="Please leave feedback"
-          onIncrementGood={handleIncrementGood}
-          onIncrementNeutral={handleIncrementNeutral}
-          onIncrementBad={handleIncrementBad}
+    <FeedbackVidgetContainer>
+      <FeedbackVidgetControls
+        title="Please leave feedback"
+        onIncrementGood={handleIncrementGood}
+        onIncrementNeutral={handleIncrementNeutral}
+        onIncrementBad={handleIncrementBad}
+      />
+      {total !== 0 ? (
+        <FeedbackVidgetStatistic
+          title="Statistic"
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={countPositiveFeedbackPercentage()}
         />
-        {total !== 0 ? (
-          <FeedbackVidgetStatistic
-            title="Statistic"
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <FeedbackVidgetNotification />
-        )}
-      </FeedbackVidgetContainer>
-)
+      ) : (
+        <FeedbackVidgetNotification />
+      )}
+    </FeedbackVidgetContainer>
+  );
 }
-
-
-
-
